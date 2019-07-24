e673d2f76f4035709b8c768d3377d69e1ee00404
kimchangkyu <kimchangkyu2@naver.com>
2019-07-24 19:19:42 +0900

    spring-post-test

---
+++ spring-post/src/main/java/aa/bb/test/SangpumBean.java
@@ -0,0 +1,30 @@
+package aa.bb.test;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SangpumBean { // 클라이언트의 다량의 정보를 클래스로 처리하겠다는 의미이다.
+	private String sang;
+	private int su, dan;
+	
+	public String getSang() {
+		return sang;
+	}
+	public void setSang(String sang) {
+		this.sang = sang;
+	}
+	public int getSu() {
+		return su;
+	}
+	public void setSu(int su) {
+		this.su = su;
+	}
+	public int getDan() {
+		return dan;
+	}
+	public void setDan(int dan) {
+		this.dan = dan;
+	}
+	
+	
+}
